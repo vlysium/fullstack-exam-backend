76bf2a9b212150311ba5d3ca42ab654bd31ae853
Victor Ly <ly.victor2000@gmail.com>
2024-06-06 13:04:21 +0200

    add comment

--- routes/authRoutes.ts
+++ routes/authRoutes.ts
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
-router.get("/user", jwtAuth, authController.getUser);
+router.get("/user", jwtAuth, authController.getUser); // route for users to get their own info
 router.delete("/user/:id", jwtAuth, adminAuth, authController.deleteUser); // route for admins to ban/delete a user
 
 export default router;
